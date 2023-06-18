6fab66d887bf34d4beda6abef8a87bc7191ad60d
pohernqii <123614940+pohernqii@users.noreply.github.com>
2023-06-18 16:17:09 +0800

    Add files via upload

---
+++ part2/Player.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Card> hand;
+    private ArrayList<Card> removedCard;
+    private int score;
+    
+    public Player() {
+        hand = new ArrayList<Card>();
+        removedCard= new ArrayList<Card>();
+    }
+
+    public void addCard(Card card) {  //adding cards in player's hand
+        hand.add(card);
+    }
+
+    public void showHand() {  
+        System.out.print(hand.toString());
+    }
+    
+    public ArrayList<Card> getCardInHand() {
+        return hand;   //return the list of player's card in hand
+    }
+    
+    public void setCardInHand(ArrayList<Card> cardInhand){
+        this.hand=cardInhand;
+    }
+    public ArrayList<Card> getRemoveCardInHand(){
+        return removedCard;
+    }
+    
+    public void setRemoveCardInHand(ArrayList<Card> removedCardInHand){
+        this.removedCard=removedCardInHand;
+    }
+
+    public void clearRemoveCardInHand(){
+        removedCard.clear();
+    }
+
+    public void clearCardInHand(){
+        hand.clear();
+    }
+	
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore(){
+        return score;
+    }
+	
+	public boolean isHandEmpty(){
+        return (hand == null || hand.size() == 0);
+    }
+
+}
